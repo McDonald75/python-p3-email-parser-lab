f72fc7255ed5f5ae891b1cfa859a3bb91ec1fc69
McDonald75 <mcdonald.omondi@student.moringaschool.com>
2024-12-15 13:58:30 +0000

    complete

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,14 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, text):
+        self.text = text
+
+    def parse(self):
+        email_regex = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+
+        potential_emails = re.split(r"[,\s]+", self.text)
+
+        valid_emails = [email for email in potential_emails if re.fullmatch(email_regex, email)]
+
+        return sorted(valid_emails)
